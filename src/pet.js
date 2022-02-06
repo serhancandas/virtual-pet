fc16f4bbda338676ff6bc41d32c2bfba6c4d63f8
serhancandas <serhancandas79@users.noreply.github.com>
2022-02-06 22:32:21 +0200

    growUp

--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,17 @@
-function Pet() {};
+function Pet(name) {
+  this.name = name;
+  this.age = 0;
+  this.hunger = 0;
+  this.fitness = 10;
+  this.growUp = function () {
+    this.age += 1;
+    this.hunger += 5;
+    this.fitness -=3;
+  };
+}
+
+const fido = new Pet('Fido');
+const rex = new Pet('Rex');
+
 
 module.exports = Pet;
--- tests/pet.test.js
+++ tests/pet.test.js
@@ -1,7 +1,61 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
+
     it('returns an object', () => {
-      expect(new Pet('Judo')).toBeInstanceOf(Object);
+      expect(new Pet('Fido')).toBeInstanceOf(Object);
+    });
+  });
+
+  describe('constructor', () => {
+
+    it('sets the name property', () => {
+      const pet = new Pet('Fido');
+  
+      expect(pet.name).toEqual('Fido');
+    });
+  });
+  
+  
+
+  describe('constructor', () => {
+  
+    it('has a initial age of 0', () => {
+      const pet = new Pet('Fido');
+  
+      expect(pet.age).toEqual(0);
+    });
+  });
+
+  
+  describe('growUp', () => {
+
+    it('increments the age by 1', () => {
+      const pet = new Pet('Fido');
+  
+      pet.growUp();
+  
+      expect(pet.age).toEqual(1);
+    });
+  });
+
+  
+  describe('growUp', () => {
+    it('increments the hunger of 5', () => {
+      const rex = new Pet('Rex');
+  
+      rex.growUp();
+  
+      expect(rex.hunger).toEqual(5);
+    });
+  });
+
+  describe('growUp', () => {
+    it('decrease the fitness by 3', () => {
+      const rex = new Pet('Rex');
+  
+      rex.growUp();
+  
+      expect(rex.fitness).toEqual(7);
     });
   });
